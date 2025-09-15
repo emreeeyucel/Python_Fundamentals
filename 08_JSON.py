a5bd2f3463961951c4bfdc35aaaa8f1455ce296c
emreeeyucel <emreyucel-34@hotmail.com>
2025-09-15 20:56:18 +0300

    Update 08_JSON.py

--- 08_JSON.py
+++ 08_JSON.py
@@ -6,7 +6,7 @@ response = get('https://newsapi.org/v2/everything?q=tesla&from=2025-08-11&sortBy
 data = response.json()
 pprint(data)
 
-# region Task 1
+
 # 0. index'den author, title ve publishedAt ifadelerini ekrana yazdıralım
 
 pprint(f' Author : {data["articles"][0]["author"]}')
@@ -17,7 +17,7 @@ pprint(f' Publis : {data["articles"][0]["publishedAt"]}')
 
 
 
-# region Task 2
+
 # Kullanıcıdan Alınan Yazar İsminin Makalesini Ekrana Yazdıralım.
 
 author_name = input('Author Name : ')
@@ -30,7 +30,7 @@ for article in data.get('articles'):
 
 
 
-# region Task 3
+
 # "Region: 'articles', 'status' ve 'totalResults' anahtarları hariç kalan veriyi bir sözlükte saklayalım ve bunu yeni bir değişkende tutalım."
 new_data = data['articles'][0]
 pprint(new_data)
@@ -38,7 +38,8 @@ pprint(new_data)
 
 
 
-# region Task 4
+
+
 # region Oluşturduğumuz 'new_data' adlı datamızdaki anahtarların isimlerini Türkçe karşılığı ile değiştirelim.
 
 # Yeni anahtarlar listesi
@@ -55,19 +56,16 @@ pprint(yeni_sozluk)
 
 
 
-# region Task 5
-# region Datamızdaki tüm anahtarların isimlerini Türkçe karşılığı ile değiştirelim.
 
+# region Datamızdaki tüm anahtarların isimlerini Türkçe karşılığı ile değiştirelim.
 haberler = []
 key_map = ['kaynak', 'yazar', 'başlık', 'tanım', 'url', 'urlResim', 'yayınlanmaTarihi', 'içerik']
 
 # Tüm makaleleri listeye ekle
-
 for makale in data['articles']:
     haberler.append(makale)
 
 # Her makale için Türkçe anahtarlarla yeni sözlük oluştur
-
 for haber in haberler:
     yeni_haber = dict(zip(key_map, haber.values()))
     pprint(yeni_haber)
@@ -75,7 +73,7 @@ for haber in haberler:
 
 
 
-# region Task 6
+
 # region toplam kaç author bulunmaktadır?
 toplam = 0
 for article in data['articles']:
@@ -88,7 +86,7 @@ for article in data['articles']:
 
 
 
-# region Task 7
+
 # region toplam kaç adet description bilgisi bulunmaktadır? (None içerir)
 toplam = 0
 for article in data['articles']:
@@ -102,7 +100,7 @@ for article in data['articles']:
 
 
 
-# region Task 8
+
 # toplam kaç author bulunmaktadır? (len formülü)
 # Boş olmayan 'author' değerine sahip makaleleri listele
 authors_with_values = []  # Boş bir liste başlatıyoruz
@@ -123,9 +121,8 @@ print(toplam)
 
 
 
-# region Task 9
-# Eğer bir haberin author bilgisi None olanları bulalım ve makaleyi yazalım
 
+# Eğer bir haberin author bilgisi None olanları bulalım ve makaleyi yazalım
 for item in data['articles']:
     if item['author'] is None:
         print(item)
@@ -134,10 +131,8 @@ for item in data['articles']:
 
 
 
-# region Task 10
+
 # Eğer bir haberin author bilgisi None ise "Unknown Author" olarak güncelleyen bir kod nasıl yazılır?
-
-
 # Path 1
 for item in data['articles']:
     if item['author'] is None:
@@ -152,7 +147,7 @@ for item in data['articles']:
 
 
 
-# region Task 11
+
 # description uzunluğu 100 karakterden fazla olan haberlerin listesini al.
 
 description_list = []
@@ -163,5 +158,109 @@ for item in data['articles']:
 
 pprint(description_list)
 print(len(description_list))
-
 # endregion
+
+
+
+
+# region Her haberin content alanındaki karakter sayısını hesaplayalım ve ortalama uzunluğu bulalım.
+import statistics
+
+content_list = []
+content_len = []
+
+for item in data['articles']:
+    content_list.append(item['content'])
+
+for item in content_list:
+    content_len.append(len(item))
+
+print(content_list)
+print(content_len)
+
+content_mean = statistics.mean(content_len)
+print(content_mean)
+# endregion
+
+
+
+
+# region Her haberin yayın tarihini (publishedAt) alıp sadece yıl bilgilerini çıkarılım ve kaç kez tekrarlandığını bulalım
+from collections import Counter
+
+publishedAt_list = []
+publishedAt_years = []
+
+for item in data['articles']:
+    publishedAt_list.append(item['publishedAt'])
+
+print(publishedAt_list)
+
+
+for i in publishedAt_list:
+    publishedAt_years.append(i[:4])
+
+print(publishedAt_years)
+print(Counter(publishedAt_years))
+# endregion
+
+
+
+
+# region Her haberin yayın tarihini (publishedAt) alıp sadece yıl bilgilerini çıkarttıktan sonra aralarına '-' işareti koyarak birleştirelim
+publishedAt_list = []
+publishedAt_years = []
+publishedAt_years_str = []
+
+for item in data['articles']:
+    publishedAt_list.append(item['publishedAt'])
+
+for i in publishedAt_list:
+    publishedAt_years.append(i[:4])
+
+for item in publishedAt_years:
+    publishedAt_years_str.append(str(item))
+
+years_birleştirme = '-'.join(publishedAt_years_str) # join metodu, listedeki öğeleri birleştirirken araya koymak istediğiniz karakteri  kullanır; örneğin, yılları birleştirirken araya '-' konursa, sonuçta '2020-2021' gibi bir string elde edilir.
+
+print(years_birleştirme)
+# joın() sadece string ifadeler kullanımı için uygundur publishedAt içindeki değerler str ifade olarak geçmemekteydi önce str çevirdik sonra sonra join ile birleştirilmiştir.
+# endregion
+
+
+
+
+# region description metnindeki tüm kelimeri sayalım ve en çok tekrar eden kelimeyi bulalım.
+from collections import Counter
+import string
+
+# Tüm description'ları listeye ekleme
+description_list = []
+
+for item in data['articles']:
+    if item['description']:
+        description_list.append(item['description'])
+
+# Tüm description'ları birleştir
+all_descriptions = ' '.join(description_list)
+
+# Noktalama işaretlerini temizle, harfleri küçült                                     # Tüm metinlerdeki noktalamaları ve harf küçültmelerde kullanılcak fuction olarak kullanılabilir.
+translator = str.maketrans('', '', string.punctuation)
+clean_text = all_descriptions.translate(translator).lower()
+
+# Kelimeleri ayır ve listeye eklenmesi
+words = clean_text.split()
+
+# Kelime frekanslarını say
+word_counts = Counter(words)
+
+# En sık geçen kelimeyi bul
+most_common_word = word_counts.most_common(1)[0]
+
+# Sonucu satır satır yazdır
+print("En sık geçen kelime:")
+print(most_common_word[0])
+print("Geçme sayısı:")
+print(most_common_word[1])
+# endregion
+
